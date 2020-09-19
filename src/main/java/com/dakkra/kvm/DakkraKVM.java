12ee08db115a6f96211ee9a49b3348b4ced3af03
dakkra <contact@dakkra.com>
2020-09-19 17:59:46 -0600

    Now using JavaFX to detect displays

--- src/main/java/com/dakkra/kvm/DakkraKVM.java
+++ src/main/java/com/dakkra/kvm/DakkraKVM.java
@@ -9,21 +9,33 @@ Project uses MIT license
 package com.dakkra.kvm;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class DakkraKVM extends Application {
+
+    private static ObservableList<Screen> displays;
 
     public static void main(String[] args) {
         System.out.println("Starting DakkraKVM...");
 
         launch();
+        if (displays.size() > 0) {
+            System.out.println("Found " + displays.size() + " displays");
+            for (Screen s : displays) {
+                System.out.println(s.toString());
+            }
+        }
 
         System.out.println("Exiting DakkraKVM");
     }
 
     @Override
     public void start(Stage primaryStage) {
-        System.out.println("Launching JavaFX...");
-        primaryStage.show();
+        System.out.println("Detecting displays...");
+        displays = Screen.getScreens();
+        Platform.exit();
     }
 }
